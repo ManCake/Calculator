0a2729c3c376f5f600fe751d780a558c622e8ab3
jmembreno4194 <jmembreno90217@gmail.com>
2021-01-06 17:37:36 -0500

    Added elif multiplication statement

--- calculator.py
+++ calculator.py
@@ -17,6 +17,12 @@ def main():
         number_2 = int(input())
         subtract(number_1, number_2)
 
+   elif operator == "*":
+        print("What 2 numbers do you want to multiply?")
+        number_1 = int(input())
+        number_2 = int(input())
+        multiply(number_1, number_2)
+
 def add(number_1, number_2):
     total = number_1 + number_2
     print("The sum is:", total)
@@ -24,6 +30,7 @@ def add(number_1, number_2):
 def subtract(number_1, number_2):
     difference = number_1 - number_2
     print("The difference is:", difference)
+
 
 # Program Flow
 main()
