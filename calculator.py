17da59a43e817720e36f5cc115408cbde8b90415
Jose Membreno <jmembreno90217@gmail.com>
2021-01-06 18:01:21 -0500

    Sick

--- calculator.py
+++ calculator.py
@@ -22,12 +22,6 @@ def main():
         number_1 = int(input())
         number_2 = int(input())
         multiply(number_1, number_2)
-    
-    elif operator == "/":
-        print("What 2 numbers do you want to divide?")
-        number_1 = int(input())
-        number_2 = int(input())
-        divide(number_1, number_2)
 
 def add(number_1, number_2):
     total = number_1 + number_2
@@ -40,6 +34,10 @@ def subtract(number_1, number_2):
 def multiply(number_1, number_2): 
     product = number_1 * number_2
     print("The product is:", product)
+
+def divide(number_1, number_2):
+    quotient = number_1 / number_2
+    print("The quotient is:", quotient)
 
 # Program Flow
 main()
