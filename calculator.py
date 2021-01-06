2268141b82c8a137271ecde4e70c85a01e203776
Jose Membreno <jmembreno90217@gmail.com>
2021-01-06 17:32:19 -0500

    added multiplication funsion

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#!/usr/bin
+
 
 def main():
     print("What type of operation?")
@@ -15,7 +15,13 @@ def add(number_1, number_2):
     total = number_1 + number_2
     print("The sum is:", total)
     
+def subtract (number_1, number_2):
+    difference = number_1 - number_2
+    print("the difference is:", difference)
+
+def multiply(number_1, number_2): 
+    product = number_1 * number_2
+    print("The product is:", product)
 
 # Program Flow
 main()
-
