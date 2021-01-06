1ca66376a50710f25e7f259daa9631f15cdd524b
jmembreno4194 <jmembreno90217@gmail.com>
2021-01-06 16:50:27 -0500

    Uploaded your calc progress

--- calculator.py
+++ calculator.py
@@ -1,1 +1,21 @@
-#!usr/bin
+#!/usr/bin
+
+def main():
+    print("What type of operation?")
+    print("(+) (-) (*) (/)")
+    operator = input()
+
+    if operator == "+":
+        print("What 2 numbers do you want to add?")
+        number_1 = int(input())
+        number_2 = int(input())
+        add(number_1, number_2)
+        
+def add(number_1, number_2):
+    total = number_1 + number_2
+    print("The sum is:", total)
+    
+
+# Program Flow
+main()
+
