72bd8e296a67123dedc6ad678fb78ffa7d37619f
Jose Membreno <jmembreno90217@gmail.com>
2021-01-06 17:40:16 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -17,7 +17,7 @@ def main():
         number_2 = int(input())
         subtract(number_1, number_2)
 
-   elif operator == "*":
+    elif operator == "*":
         print("What 2 numbers do you want to multiply?")
         number_1 = int(input())
         number_2 = int(input())
